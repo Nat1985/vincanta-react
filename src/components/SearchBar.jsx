52aa1af458a7b30edfd932bf9d93f74488b10de6
Andrea Natale <andreanatale@192.168.1.50>
2024-03-11 18:08:36 +0100

    Searchbar updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,14 +11,16 @@ const SearchBar = () => {
         console.log('searchInput: ', searchInput )
     }, [searchInput])
     return (
-        <div className="fixed top-0 left-0 flex gap-2 items-center py-2 px-4 rounded bg-fuchsia-50">
-            {!isActive && <div className="pt-[5px]"><i class="fi fi-br-search text-[#782a76] cursor-pointer" onClick={() => setIsActive(true)}></i></div>}
+        <div className={`fixed ${isActive ? 'top-2 bg-fuchsia-50 py-2 px-4 ' : 'top-1 left-1 bg-[#782a76] py-1 px-3 '} flex gap-2 items-center rounded-lg`}>
+            {!isActive && <div className="pt-[5px]"><i class="fi fi-br-search  text-white cursor-pointer" onClick={() => setIsActive(true)}></i></div>}
             {
                 isActive &&
                 <div className="flex gap-2 items-center">
                     <div className="pt-[8px]"><i class="fi fi-ss-circle-xmark text-end text-[#782a76] cursor-pointer" onClick={() => setIsActive(false)}></i></div>
                     <h4 className="font-thin">Ricerca vino</h4>
+                    <div className="flex flex-col">
                     <input type="text" className="rounded border" onChange={handleInput}/>
+                    </div>
                     <div className="pt-[5px] ml-2"><i class="fi fi-br-search text-[#782a76] cursor-pointer"></i></div>
                 </div>
             }
