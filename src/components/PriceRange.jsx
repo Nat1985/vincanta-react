9405ede37196a2d1e22cacb401691afdcb4cb221
Andrea Natale <andreanatale@MacBook-Pro-di-Andrea.local>
2024-05-26 17:41:19 +0200

    PriceRange layout updated

--- src/components/PriceRange.jsx
+++ src/components/PriceRange.jsx
@@ -28,19 +28,19 @@ const PriceRange = () => {
     }
     return (
         <div className="flex flex-col gap-2 p-4 rounded-xl border border-[#782a76] items-center">
-            <h4>Imposta un range di prezzo</h4>
+            {/* <h4>Imposta un range di prezzo</h4> */}
             <div className="flex gap-2 items-center">
                 <label htmlFor="from">Da:</label>
                 <input type="number" id="from" className="w-24" onChange={handlePriceInput} value={priceInput ? priceInput.from : ''} />
                 <label htmlFor="to">a:</label>
                 <input type="number" id="to" className="w-24" onChange={handlePriceInput} value={priceInput ? priceInput.to : ''} />
-            <NoBgButton text="Imposta" click={handleSetRange} />
+            <div className="text-sm"><NoBgButton text="Imposta intervallo di prezzo" click={handleSetRange} /></div>
             </div>
             {
                 priceRange &&
                 <div className="flex gap-2 border font-bold w-fit py-1 px-2">
                     <div>Range:</div>
-                    <div>{priceRange.from} - {priceRange.to}</div>
+                    <div>{priceRange.from}€ - {priceRange.to}€</div>
                     <i class="fi fi-ss-circle-xmark text-end text-[#782a76] cursor-pointer mt-[3px]" onClick={handleRemoveRange}></i>
                 </div>
             }
