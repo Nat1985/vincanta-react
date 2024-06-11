3e071330d85770cc3a474a7c17a8acf86eafc0fb
Andrea Natale <andreanatale@iMac-di-Andrea.local>
2024-06-11 12:23:35 +0200

    Champagne category layout updated

--- src/components/CatLabel.jsx.jsx
+++ src/components/CatLabel.jsx.jsx
@@ -1,6 +1,6 @@
 const CatLabel = ({cat}) => {
     return (
-        <div className="border border-[#782a76] rounded text-xs flex items-center p-1 ml-1 mb-1">
+        <div className="border border-[#782a76] h-fit w-fit rounded text-xs flex items-center px-1 mb-2 md:mb-[-4px]">
         {cat}
         </div>
     )
--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,16 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import WineLine from "./WineLine";
 import { useSelector } from "react-redux";
+import CatLabel from "./CatLabel.jsx";
 
 const CompanyCard = ({ data }) => {
     const { priceRange } = useSelector(state => state.query);
+
+    // Imposto la categoria champagne se presente nel WineLine (prendendo soltanto il primo)
+    const [champagneCategory, setChampagneCategory] = useState(null);
+    useEffect(() => {
+        if(data) {
+            // Catturo la champagneCategory solo del primo
+            for(let wine of data.data) {
+                if(wine.champagneCategory) {
+                    setChampagneCategory(wine.champagneCategory);
+                    break;
+                }
+            }
+        }
+    }, [data])
     return (
         <div className="w-[320px] md:w-full flex flex-col gap-2 p-4 border-2 rounded">
 
             {/* Azienda e paese */}
-            <div className="flex flex-col md:flex-row gap-2 text-start border-b">
+            <div className="flex flex-col md:flex-row md:items-center gap-2 text-start border-b">
                 <div className="flex items-center"><h3>{data.company}</h3></div>
                 <div className="flex items-center italic"><span className="hidden md:block">-</span><h4 className="ml-2">{data.data[0].city}</h4></div>
+                {champagneCategory && <CatLabel cat={champagneCategory} />}
             </div>
 
             {/* Elenco vini */}
